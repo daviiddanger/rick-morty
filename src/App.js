64f0e583e30af0944ece82ad908261cf8ec28007
Ing. David Morales Juarez <94495486+daviiddanger@users.noreply.github.com>
2024-04-18 11:51:50 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import imageRickMorty from './img/rick-morty.png';
+import './App.css';
+import { useState } from "react";
+import Characters from './components/Characters';
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () =>{
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+  };
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ):(
+        <>
+        <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+        <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+        
+        </>
+        
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
